c9eae43821229cebc73852e300a89b69f838cdaf
yevhen-poda <poda.mailbox@gmail.com>
2024-02-16 19:26:43 +0100

    Es wurde eine responsive Version des Headers für mobile Geräte hinzugefügt.

--- js/main/menu.js
+++ js/main/menu.js
@@ -1,15 +1,29 @@
 'use strict'
 
+document.body.classList.remove('no-js')
+
+const menu = document.querySelector('.menu')
+const menuToggle = document.querySelector('.header__menu-toggle')
+
+if (menuToggle) {
+  menuToggle.addEventListener('click', function(e) {
+    if (menu) {
+      e.currentTarget.classList.toggle('header__menu-toggle--active')
+      document.body.classList.toggle('menu-opened')
+    }
+  })
+}
+
 const navList = document.querySelector('.nav__list')
 
 if (navList) {
-  navList.addEventListener('click', function(evt) {
-    const curTarget = evt.currentTarget
-    const target = evt.target
+  navList.addEventListener('click', function(e) {
+    const curTarget = e.currentTarget
+    const target = e.target
     const activeLink = 'nav__link--active'
     if (target.classList.contains('nav__link')) {
       curTarget.querySelector('.' + activeLink).classList.remove(activeLink)
       target.classList.add(activeLink)
     }
-  });
+  })
 }
